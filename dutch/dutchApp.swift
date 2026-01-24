5df95f4e67b6306dc100635583e6b7c854efd38c
shreyanshu005 <shreyanshu005@gmail.com>
2026-01-24 11:30:30 +0530

    Initial Commit

---
+++ dutch/dutchApp.swift
@@ -0,0 +1,17 @@
+//
+//  dutchApp.swift
+//  dutch
+//
+//  Created by Shreyanshu on 24/01/26.
+//
+
+import SwiftUI
+
+@main
+struct dutchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dutchTests/dutchTests.swift
@@ -0,0 +1,17 @@
+//
+//  dutchTests.swift
+//  dutchTests
+//
+//  Created by Shreyanshu on 24/01/26.
+//
+
+import Testing
+@testable import dutch
+
+struct dutchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
